e34d15ed40b172541deefda708b74d14d0aebca7
Leo Fong <leofong@iPad-7.localdomain>
2024-04-30 00:10:00 -0400

    added arrow functionality and less intense scrolling

--- src/Camera2D.cs
+++ src/Camera2D.cs
@@ -12,20 +12,20 @@ public partial class Camera2D : Godot.Camera2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
-        float movementStep = 5f;
-        if (Input.IsKeyPressed(Key.W))
+        float movementStep = 10f;
+        if (Input.IsKeyPressed(Key.W) || Input.IsKeyPressed(Key.Up))
         {
             this.Position += new Godot.Vector2(0f, -movementStep);
         }
-        if (Input.IsKeyPressed(Key.A))
+        if (Input.IsKeyPressed(Key.A) || Input.IsKeyPressed(Key.Left))
         {
             this.Position += new Godot.Vector2(-movementStep, 0f);
         }
-        if (Input.IsKeyPressed(Key.S))
+        if (Input.IsKeyPressed(Key.S) || Input.IsKeyPressed(Key.Down))
         {
             this.Position += new Godot.Vector2(0f, movementStep);
         }
-        if (Input.IsKeyPressed(Key.D))
+        if (Input.IsKeyPressed(Key.D) || Input.IsKeyPressed(Key.Right))
         {
             this.Position += new Godot.Vector2(movementStep, 0f);
         }
@@ -40,12 +40,12 @@ public partial class Camera2D : Godot.Camera2D
             {
                 //Activates when mouse wheel goes up
                 case MouseButton.WheelUp:
-                    Zoom = Zoom * 2; // coded by Leo
+                    Zoom = Zoom * 1.5f; // coded by Leo
                     break;
 
                 //Activates when mouse wheel goes down
                 case MouseButton.WheelDown:
-                    Zoom = Zoom / 2;
+                    Zoom = Zoom / 1.5f;
                     break;
             }
         }
